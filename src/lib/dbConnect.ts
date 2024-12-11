1d093311b43ded5c7144a24bde543de4ba55f0d1
DEBJIT <0508debjitkundu2005@gmail.com>
2024-12-11 21:59:09 +0530

    added database connection and resend email connection

---
+++ src/lib/dbConnect.ts
@@ -0,0 +1,36 @@
+import mongoose from 'mongoose';
+
+type ConnectionObject = {
+  //returned is optional so ? but if returned it is number 
+  isConnected?: number;
+};
+
+const connection: ConnectionObject = {};
+
+//check for promise<void> type
+//void means we dont care abt which type of data is returned
+async function dbConnect(): Promise<void> {
+  // Check if we have a connection to the database or if it's currently connecting
+  if (connection.isConnected) {
+    console.log('Already connected to the database');
+    return;
+  }
+
+  try {
+    // Attempt to connect to the database
+    //STUDY MONGOOSE WHAT MORE OBJECT CAN BE SEND IN MONGOOSE.CONNECT
+    const db = await mongoose.connect(process.env.MONGODB_URI || '', {});
+
+    
+    connection.isConnected = db.connections[0].readyState;
+
+    console.log('Database connected successfully');
+  } catch (error) {
+    console.error('Database connection failed:', error);
+
+    // Graceful exit in case of a connection error
+    process.exit(1);
+  }
+}
+
+export default dbConnect;
