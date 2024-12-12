be2025c05861152081daf21cad7ee7501fdaa2d3
DEBJIT <0508debjitkundu2005@gmail.com>
2024-12-12 23:17:47 +0530

    added the email template

---
+++ emails/verificationEmail.tsx
@@ -0,0 +1,67 @@
+//maybe do some better styles 
+
+import {
+    Html,
+    Head,
+    Font,
+    Preview,
+    Heading,
+    Row,
+    Section,
+    Text,
+    Button,
+  } from '@react-email/components';
+  
+  interface VerificationEmailProps {
+    username: string;
+    otp: string;
+  }
+  
+  export default function VerificationEmail({ username, otp }: VerificationEmailProps) {
+    return (
+      <Html lang="en" dir="ltr">
+        <Head>
+          <title>Verification Code</title>
+          <Font
+            fontFamily="Roboto"
+            fallbackFontFamily="Verdana"
+            webFont={{
+              url: 'https://fonts.gstatic.com/s/roboto/v27/KFOmCnqEu92Fr1Mu4mxKKTU1Kg.woff2',
+              format: 'woff2',
+            }}
+            fontWeight={400}
+            fontStyle="normal"
+          />
+        </Head>
+        <Preview>Here&apos;s your verification code: {otp}</Preview>
+        <Section>
+        {/* The <Row> component is a layout component from @react-email/components that helps structure email content in horizontal rows. It's similar to a div with display: flex. */}
+          <Row>
+            <Heading as="h2">Hello {username},</Heading>
+          </Row>
+          <Row>
+            <Text>
+              Thank you for registering. Please use the following verification
+              code to complete your registration:
+            </Text>
+          </Row>
+          <Row>
+            <Text>{otp}</Text> 
+          </Row>
+          <Row>
+            <Text>
+              If you did not request this code, please ignore this email.
+            </Text>
+          </Row>
+          {/* <Row>
+            <Button
+              href={`http://localhost:3000/verify/${username}`}
+              style={{ color: '#61dafb' }}
+            >
+              Verify here
+            </Button>
+          </Row> */}
+        </Section>
+      </Html>
+    );
+  }
